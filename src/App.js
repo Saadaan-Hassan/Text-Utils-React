ff7ba05b5187d0a5865434880b2c6dc9cd71c8e2
Saadaan <webmaker9d@gmail.com>
2022-09-11 15:00:25 +0500

    Add darkmode to navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import React, { useState } from 'react'
+
 
 function App() {
+  const [mode, setMode] = useState("light")
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode("dark")
+    }
+    else{
+      setMode("light")
+    }
+  }
+
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar mode={mode} toggleMode={toggleMode}/>
     </div>
   );
 }
