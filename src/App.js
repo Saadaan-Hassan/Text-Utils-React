ed37427d95e1fc8397d6055c042c0ea1c141b515
Saadaan <webmaker9d@gmail.com>
2022-09-11 16:11:47 +0500

    Add different utility functions
    Add different utility functions to perform on the text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import './App.css';
-import Navbar from './Components/Navbar';
-import React, { useState } from 'react'
-import TextForm from './Components/TextForm';
-
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import React, { useState } from "react";
+import TextForm from "./Components/TextForm";
 
 function App() {
-  const [mode, setMode] = useState("light")
+  const [mode, setMode] = useState("light");
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
-      setMode("dark")
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+    } else {
+      setMode("light");
     }
-    else{
-      setMode("light")
-    }
-  }
+  };
 
   return (
-    <div className="App">
-      <Navbar mode={mode} toggleMode={toggleMode}/>
-      <TextForm/>
+    <div
+      className="App"
+      style={{ backgroundColor: mode === "light" ? "white" : "gray", color: mode ==="light"? "black":"white" }}
+    >
+      <Navbar mode={mode} toggleMode={toggleMode} />
+      <div className="container">
+        <TextForm mode={mode}/>
+      </div>
     </div>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,22 +1,81 @@
 import React from "react";
+import { useState } from "react";
 
-export default function TextForm() {
+export default function TextForm(props) {
+  const [text, setText] = useState("");
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const upperCase = () => {
+    setText(text.toUpperCase());
+  };
+
+  const lowerCase = () => {
+    setText(text.toLowerCase());
+  };
+
+  const titleCase = () => {
+    let lower = text
+      .toLowerCase()
+      .split(" ")
+      .map((element) => {
+        return element.charAt(0).toUpperCase() + element.slice(1);
+      })
+      .join(" ");
+    setText(lower);
+  };
+
+  const removeExtraSpaces = () => {
+    let newText = text.split(/[ ]+/)
+    setText(newText.join(" "));
+  };
+
+  const copyText = () => {
+    let input = document.getElementById("textArea");
+    input.select();
+    navigator.clipboard.writeText(input.value);
+  };
+
+  const clearText = () => {
+    setText("");
+  };
+
   return (
-    <div className="container">
+    <>
       <div className="mb-3">
         <h1>Enter Your Text Here</h1>
         <textarea
           className="form-control"
+          value={text}
+          onChange={handleChange}
           id="textArea"
           rows="8"
+          style={{
+            backgroundColor: props.mode === "light" ? "white" : "gray",
+            color: props.mode === "light" ? "black" : "white",
+          }}
         ></textarea>
       </div>
-      <button className="btn btn-success mx-1">Upper Case</button>
-      <button className="btn btn-success mx-1">Lower Case</button>
-      <button className="btn btn-success mx-1">Title Case</button>
-      <button className="btn btn-success mx-1">Remove Extra Spaces</button>
-      <button className="btn btn-success mx-1">Copy Text</button>
-      <button className="btn btn-success mx-1">Clear Text</button>
-    </div>
+      <button className="btn btn-success mx-1" onClick={upperCase}>
+        Upper Case
+      </button>
+      <button className="btn btn-success mx-1" onClick={lowerCase}>
+        Lower Case
+      </button>
+      <button className="btn btn-success mx-1" onClick={titleCase}>
+        Title Case
+      </button>
+      <button className="btn btn-success mx-1" onClick={removeExtraSpaces}>
+        Remove Extra Spaces
+      </button>
+      <button className="btn btn-success mx-1" onClick={copyText}>
+        Copy Text
+      </button>
+      <button className="btn btn-success mx-1" onClick={clearText}>
+        Clear Text
+      </button>
+    </>
   );
 }
